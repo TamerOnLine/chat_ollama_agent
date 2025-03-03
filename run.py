76c90340a9a9a988ef54ff751fee2d41b7a0bd91
TamerOnLine <tamer@tameronline.com>
2025-03-03 09:36:25 +0100

    run main

--- run.py
+++ run.py
@@ -2,31 +2,21 @@ import os
 import sys
 import subprocess
 
-
 def check_python():
     """Check if Python is installed."""
     if sys.version_info.major < 3:
         print("Python 3 is required to run this program.")
         sys.exit(1)
 
-
 def install_requirements():
     """Automatically install required dependencies."""
     print(" Installing requirements...")
     subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
 
-
 def run_app():
-    """Run the application."""
-    print(" Running the app on http://127.0.0.1:8000/")
-    subprocess.run(
-        [
-            sys.executable, "-m", "uvicorn", "api:app",
-            "--host", "0.0.0.0", "--port", "8000", "--reload"
-        ],
-        check=True
-    )
-
+    """Run the main.py script."""
+    print(" Running main.py ...")
+    subprocess.run([sys.executable, "src/main.py"], check=True)
 
 if __name__ == "__main__":
     check_python()
